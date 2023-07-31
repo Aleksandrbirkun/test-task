23d373a435c488e3f8b27bb5354fed4df94f2dca
KNIDZE <mihaknidze@gmail.com>
2023-07-31 14:50:16 +0300

    TestTask

--- src/Compontents/CardMedia/CardMedia.tsx
+++ src/Compontents/CardMedia/CardMedia.tsx
@@ -1,13 +1,31 @@
-import {Card} from "@mui/material";
-import React from "react";
-import cardMedia from './CardMedia.png';
+import {Box, Card, Checkbox, FormControlLabel} from "@mui/material";
+import React, {useState} from "react";
+import cardMedia from "./CardMedia.png";
 
+const hoveredStyle = {
+  border: "2px solid #65E9D9"
+}
+const cardStyle = {
+  width: 229,
+  height: 150,
+  borderRadius: "10px",
+  border: "2px solid transparent",
+  "&:hover": {hoveredStyle}
+}
 
-const ZoneBox = (): JSX.Element => {
+export default function CardMedia({label, image=cardMedia}: { label: string, image: string }): React.ReactElement {
+  const [isChecked, setChecked] = useState(false);
   return (
-    <Card variant="outlined">
-      <img src={cardMedia} alt='cardMedia.png'/>
+    <Card variant="outlined" sx={ isChecked ? {...cardStyle, ...hoveredStyle} : cardStyle } >
+      <img src={image} alt="cardMedia.png"/>
+        <FormControlLabel
+          control={<Checkbox value={isChecked}
+                             sx={{
+                               "&.Mui-checked": {
+                                 color: "#21B6A8",
+                               }
+                             }}
+                             onChange={() => setChecked((pr) => !pr)}/>} label={label} sx={{marginLeft:'8px'}}/>
     </Card>
   )
 }
-export default ZoneBox;
